cc8b707de8f10c12ba475041b4b46f217021674e
a-humanthing <arunkrishnakt2002@gmail.com>
2023-06-20 11:26:14 +0530

    assigned enum valudation for passiolevel field

--- model/Hobby.ts
+++ model/Hobby.ts
@@ -1,10 +1,18 @@
 import mongoose from "mongoose"
 
 const Schema = mongoose.Schema
-const hobbySchema = new Schema({
-  passionLevel: { type: String, required: true },
-  name: { type: String, required: true },
-  year: { type: Number, required: true },
-})
+const hobbySchema = new Schema(
+  {
+    passionLevel: {
+      type: String,
+      enum: ["Low", "Medium", "High", "Very-High"],
+      default: "Medium",
+      required: true,
+    },
+    name: { type: String, required: true },
+    year: { type: Number, required: true },
+  },
+  { strict: "throw" }
+)
 
 export default mongoose.model("Hobby", hobbySchema)
